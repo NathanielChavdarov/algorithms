9c9b21c67b7e99a270f268cb7ec4cc4bc57a528a
Nathaniel <nathanielchavdarov09@gmail.com>
2021-09-28 18:07:49 +0100

    Add source #8

---
+++ algorithms/fibonacci/fibonacci.py
@@ -0,0 +1,12 @@
+def fib(n: int) -> int:
+    if n == 0:
+        return 0
+    y = 1
+    z = 1
+    x = 2
+    for i in range(n-2):
+        y = x
+        x = y + z
+        z = y
+
+    return z
