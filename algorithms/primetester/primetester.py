e4fa7a565ecd3471de3676b6265ba263e8ffb49a
Nathaniel <nathanielchavdarov09@gmail.com>
2021-10-13 17:53:48 +0100

    Complete source (#29)

--- algorithms/primetester/primetester.py
+++ algorithms/primetester/primetester.py
@@ -3,3 +3,11 @@ def primetester2(a: int) -> bool:
         return a > 1
     if a % 2 == 0 or a % 3 == 0:
         return False
+
+    i = 5
+
+    while i * i <= a:
+        if a % i == 0 or a % (i+2) == 0:
+            return False
+        i += 6
+    return True
