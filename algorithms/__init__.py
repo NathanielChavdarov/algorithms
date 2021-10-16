4231f975f8a86d47ef1713b0db6b7519ee8e00c5
Nathaniel <nathanielchavdarov09@gmail.com>
2021-10-16 12:38:02 +0100

    Implement mean algorithm and tests (#38)

--- algorithms/__init__.py
+++ algorithms/__init__.py
@@ -1,6 +1,7 @@
 from .bubble_sort import bubble_sort  # noqa: F401
 from .factorial import factorial_iterative, factorial_recursive  # noqa: F401
 from .fibonacci import fib  # noqa: F401
+from .mean import mean  # noqa: F401
 from .picalc import picalc  # noqa: F401
 from .primetester import primetester  # noqa: F401
 from .sqrt import sqrt  # noqa: F401
---
+++ algorithms/mean/mean.py
@@ -0,0 +1,5 @@
+from typing import List, Union
+
+
+def mean(inlist: List[Union[float, int]]) -> float:
+	return sum(inlist) / len(inlist)
