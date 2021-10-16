5143238de93ed38659445d311652dedd17909bc4
Nathaniel <nathanielchavdarov09@gmail.com>
2021-10-16 12:03:15 +0100

    Improve picalc test speed (#35)

--- tests/picalc/test_picalc.py
+++ tests/picalc/test_picalc.py
@@ -6,7 +6,7 @@ def test_picalc():
         (10, 3, 4),
         (100, 3.1, 3.2),
         (1000, 3.14, 3.15),
-        (5000, 3.142, 3.143),
+        # (5000, 3.142, 3.143),
     ]
     for i, l, u in answers:
         assert picalc(i) > l and picalc(i) < u
