fa0f9a5805c268683bdd4e562704e7618c8d60b3
Nathaniel <nathanielchavdarov09@gmail.com>
2021-10-16 11:54:54 +0100

    Fix primetester (#34)

--- algorithms/__init__.py
+++ algorithms/__init__.py
@@ -1,5 +1,6 @@
 from .bubble_sort import bubble_sort  # noqa: F401
-from .factorial import factorial_iterative, factorial_recursive  # noqa:F401
-from .fibonacci import fib  # noqa:F401
-from .picalc import picalc  # noqa:F401
+from .factorial import factorial_iterative, factorial_recursive  # noqa: F401
+from .fibonacci import fib  # noqa: F401
+from .picalc import picalc  # noqa: F401
+from .primetester import primetester  # noqa: F401
 from .sqrt import sqrt  # noqa: F401
