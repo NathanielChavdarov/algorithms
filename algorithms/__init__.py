e1d92af1be6e92b811f2020fb3d822a85292e45c
Nathaniel <nathanielchavdarov09@gmail.com>
2021-10-19 09:14:59 +0100

    Fix flake8 error

--- algorithms/__init__.py
+++ algorithms/__init__.py
@@ -1,5 +1,5 @@
 from .bubble_sort import bubble_sort  # noqa: F401
-from .div import div # noqa: F401
+from .div import div  # noqa: F401
 from .factorial import factorial_iterative, factorial_recursive  # noqa: F401
 from .fibonacci import fib  # noqa: F401
 from .mean import mean  # noqa: F401
