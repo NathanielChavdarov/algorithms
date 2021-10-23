af8b4542a18fe5a5f5cd8aa0ce248419e891fb5f
Nathaniel <nathanielchavdarov09@gmail.com>
2021-10-23 18:25:32 +0100

    Speed up standard deviation (#43)

--- algorithms/standard_deviation/standard_deviation.py
+++ algorithms/standard_deviation/standard_deviation.py
@@ -1,10 +1,14 @@
+import random
 from math import sqrt
 from typing import List, Union
 
+import timeit
 
 def standard_deviation(inlist: List[Union[float, int]]) -> float:
+    mean = (sum(inlist) / len(inlist))
+    dividend = len(inlist) - 1
     deviation2 = sqrt(
-        sum([(x - sum(inlist) / len(inlist)) ** 2 for x in inlist])
-        / (len(inlist) - 1)
+        sum([(x - mean) ** 2 for x in inlist])
+        / dividend
     )
     return round(deviation2, 3)
