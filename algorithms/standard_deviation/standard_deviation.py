ecc2d5132c39a6373b804b923285b565e11c9644
Nathaniel <nathanielchavdarov09@gmail.com>
2021-10-23 18:28:57 +0100

    Run black on source (#43)

--- algorithms/standard_deviation/standard_deviation.py
+++ algorithms/standard_deviation/standard_deviation.py
@@ -3,10 +3,7 @@ from typing import List, Union
 
 
 def standard_deviation(inlist: List[Union[float, int]]) -> float:
-    mean = (sum(inlist) / len(inlist))
+    mean = sum(inlist) / len(inlist)
     dividend = len(inlist) - 1
-    deviation2 = sqrt(
-        sum([(x - mean) ** 2 for x in inlist])
-        / dividend
-    )
+    deviation2 = sqrt(sum([(x - mean) ** 2 for x in inlist]) / dividend)
     return round(deviation2, 3)
